7bf7437a8dc51ceb993c2d59b3d7d46da06c6fdb
McKynlee Westman <mckynlee.westman@gmail.com>
2021-04-07 20:40:49 -0500

    changed all pre-set "user" tables to "users"

--- database.sql
+++ database.sql
@@ -3,11 +3,17 @@
 -- You must use double quotes in every query that user is in:
 -- ex. SELECT * FROM "user";
 -- Otherwise you will have errors!
-CREATE TABLE "user" (
+CREATE TABLE "users" (
     "id" SERIAL PRIMARY KEY,
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
+
+
+
+
+
+
 
 
 
