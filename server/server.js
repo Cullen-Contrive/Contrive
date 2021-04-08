eeddee98f1c35d0f6ec5ffe008e2c415d323efa8
Jason Lin <jasonwl_1995@yahoo.com>
2021-04-08 18:07:11 -0500

    aws linked up

--- server/server.js
+++ server/server.js
@@ -9,6 +9,22 @@ const passport = require('./strategies/user.strategy');
 
 // Route includes
 const userRouter = require('./routes/user.router');
+const awsRouter = require('./routes/aws.router');
+
+// TODO AWS const
+const UploaderS3Router = require('react-dropzone-s3-uploader/s3router')
+
+// AWS Configuration //
+app.use('/s3', UploaderS3Router ({
+  bucket: 'cullen-contrive',                // required
+  region: 'us-east-2',                            // optional
+  // bucket: process.env.AWS_S3_BUCKET,           // required
+  // region: process.env.AWS_S3_REGION,           // optional
+  headers: {'Access-Control-Allow-Origin': '*'},  // optional
+  // ACL: 'private',                                 // this is the default - set to `public-read` to let anyone view uploads
+  ACL: 'public-read',
+  signatureVersion: 'v4',
+}));
 
 // Body parser middleware
 app.use(bodyParser.json());
@@ -23,6 +39,7 @@ app.use(passport.session());
 
 /* Routes */
 app.use('/api/user', userRouter);
+app.use('/api/aws', awsRouter);
 
 // Serve static files
 app.use(express.static('build'));
