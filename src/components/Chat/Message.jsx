67c9ba67b8c4ed4e7dd6dd0ad1e98d3fd1abd8b8
Johnshandsable <Jshands@asu.edu>
2021-04-11 10:41:08 -0500

    feature-chat-endtoend

--- src/components/Chat/Message.jsx
+++ src/components/Chat/Message.jsx
@@ -6,7 +6,9 @@ function Message({ messageDetails }) {
   console.log('made it');
 
   return (
-    // Conditionally render to or from based on what the req.user.id is
+    // Conditionally render to or from based on what the user.id is
+    // messageDetails.fromUser === currently logged in user id
+    // Should set it up to render on left or right side
     <div className="MessageContainer">
       <Typography>{messageDetails.message}</Typography>
     </div>
