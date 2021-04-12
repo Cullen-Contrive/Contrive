d5d4cbae955b24aac69def9082ddf6cea686969b
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-04-12 11:17:43 -0500

    nav sticks to bottom page

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,10 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
+import useStyles from './Nav.styles'
 import {useSelector} from 'react-redux';
 
+// Material-UI Components
+import {
+  BottomNavigation,
+  BottomNavigationAction
+} from '@material-ui/core';
+
+// Material-UI Icons
+import LanguageIcon from '@material-ui/icons/Language';
+import SearchIcon from '@material-ui/icons/Search';
+import AddCircleIcon from '@material-ui/icons/AddCircle';
+import ChatIcon from '@material-ui/icons/Chat';
+import MenuIcon from '@material-ui/icons/Menu';
+
 function Nav() {
+  const classes = useStyles();
+  const [value, setValue] = useState('discover')
   const user = useSelector((store) => store.user);
 
   let loginLinkData = {
@@ -16,31 +32,46 @@ function Nav() {
     loginLinkData.path = '/user';
     loginLinkData.text = 'Home';
   }
+  
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  }
 
   return (
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
-        <Link className="navLink" to={loginLinkData.path}>
-          {loginLinkData.text}
-        </Link>
+    // <div className="nav">
+    //   <Link to="/home">
+    //     <h2 className="nav-title">Prime Solo Project</h2>
+    //   </Link>
+    //   <div>
+    //     <Link className="navLink" to={loginLinkData.path}>
+    //       {loginLinkData.text}
+    //     </Link>
 
-        {user.id && (
-          <>
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-            <LogOutButton className="navLink" />
-          </>
-        )}
+    //     {user.id && (
+    //       <>
+    //         <Link className="navLink" to="/info">
+    //           Info Page
+    //         </Link>
+    //         <LogOutButton className="navLink" />
+    //       </>
+    //     )}
 
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div>
-    </div>
+    //     <Link className="navLink" to="/about">
+    //       About
+    //     </Link>
+    //   </div>
+    // </div>
+    <BottomNavigation
+      value={value}
+      onChange={handleChange}
+      className={classes.navbar}
+    >
+      <BottomNavigationAction label="Discover" value="discover" icon={<LanguageIcon />} />
+      <BottomNavigationAction label="Search" value="search" icon={<SearchIcon />} />
+      <BottomNavigationAction label="Plan" value="plan" icon={<AddCircleIcon />} />
+      <BottomNavigationAction label="Messages" value="messages" icon={<ChatIcon />} />
+      <BottomNavigationAction label="Menu" value="menu" icon={<MenuIcon />} />
+    </BottomNavigation>
   );
 }
 
---
+++ src/components/Nav/Nav.styles.js
@@ -0,0 +1,12 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles({
+  navbar: {
+    overflow: 'hidden',
+    position: 'fixed',
+    bottom: 0,
+    width: '100%'
+  }
+})
+
+export default useStyles;
