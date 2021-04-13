02f6413037d14d824f19aa2415e76a91d4cdb0c2
McKynlee Westman <mckynlee.westman@gmail.com>
2021-04-13 14:53:29 -0500

    added a space

---
+++ src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const SearchBar = ({ keyword, setKeyword }) => {
+  const BarStyling = { width: "22rem", background: "#F2F1F9", border: "1px solid", borderRadius: "7px", padding: "0.5rem" };
+  return (
+    <input
+      style={BarStyling}
+      key="random1"
+      value={keyword}
+      placeholder={`Search vendors by name e.g. "Kiki's Delivery Service"`}
+      onChange={(i) => setKeyword(i.target.value)}
+    />
+  );
+}
+export default SearchBar;
