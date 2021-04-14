da39761b0d8879f2c78bdf645c30c7d98faa0a3f
William Krug <william.p.krug@gmail.com>
2021-04-14 16:39:49 -0500

    "vendors_services" table updated

--- dummyData.sql
+++ dummyData.sql
@@ -7,7 +7,121 @@ VALUES ('Mac@gmail.com', '$2a$10$d.tw3AyrHON3AynvQDuoN.my8uxemJ7KZhzc7fcRMAHiuMp
 ('LilRusty@gmail.com', '$2a$10$nxWzD4fOt4n72TbnMlYXQO/yZknXnREW7s9DJY9IP0dIJG2PzsbiW', 'Dusty', 'Meyers', '77 DnD Blvd', 'Lawrence', 'KS', 66044, 'planner', 'https://www.imdb.com/title/tt0190374/', 'Looking to plan the wedding of my lifetime!'),
 ('JDog@gmail.com', '$2a$10$vSH/PXClWlf3vPlHKRLNXu5TRQPamdzbn921Q7tgtiDsK20SVTB2m', 'John', 'Shands', '69 Woodstock Lane', 'Liberty', 'MO', 64068, 'vendor', 'https://www.libertymissouri.gov/1500/Liberty-Summer-Band', 'Able to provide a musical experience that your guests will never forget.'),
 ('RedRebel@gmail.com', '$2a$10$coxFv5Ll23qca8Dp4LZ9T.UNlQTDQ32BM1FHXrKVqNwEJVmwWs8hG', 'Jason', 'Lin', '123 Four Ave', 'St. Cloud', 'MN', 56301, 'planner', 'https://memes.com/', 'Helping local artists plan gallery-openings throughout the Twin Cities.'),
-('MushroomMan@gmail.com', '$2a$10$y1VJWv.1EEpl5cbpp8csguhA8OuXfou32w/wWTA.yrOzrmv6i2zMW', 'Edan', 'Schwartz', '1 Magic Mushy Rd', 'Minneapolis', 'MN', 55404, 'vendor', 'https://www.birdsandblooms.com/stuff-we-love/gardening-stuff/mushroom-growing-kits/', 'Mushroom-catering specialty; guaranteed to serve only the non-toxic kind.');
+('MushroomMan@gmail.com', '$2a$10$y1VJWv.1EEpl5cbpp8csguhA8OuXfou32w/wWTA.yrOzrmv6i2zMW', 'Edan', 'Schwartz', '1 Magic Mushy Rd', 'Minneapolis', 'MN', 55404, 'vendor', 'https://www.birdsandblooms.com/stuff-we-love/gardening-stuff/mushroom-growing-kits/', 'Mushroom-catering specialty; guaranteed to serve only the non-toxic kind.')
+('Mintahoe', 'one', 'Jackie', 'Chan', '2850 Anthony Lane South', 'Minneapolis', 'MN', 55418, 'vendor', 'https://www.mintahoe.com/', 'When you choose Mintahoe Catering &  Events, you choose pease of mind.  Our event experts will help you design the perfect menu with your vision and budget. Envision the Wow.'),
+('Chowgirls', 'one', 'Annie', 'Oakley', '336 Hoover St NE', 'Minneapolis', 'MN', 55413, 'vendor', 'https://www.chowgirls.net/', 'Whether you are celebrating a grad, a dad, or just have plans for simple al fresco dining this spring and summer, we have got delicious ways to celebrate safely.'),
+('Naf Naf Middle Eastern Grill', 'one', 'Amir', 'Abdul', '3340 Central Park Village Drive, Suite 100', 'Egan', 'MN', 55121, 'vendor', 'https://www.nafnafgrill.com/', 'We promise to be your guide to exploring the best flavors of the Middle East each time we make our handmade dishes, like pillowy pita, crispy falafel, savory shawarma, and our scratch-made signature sauces.'),
+('CRAVE Catering & Events', 'one', 'Domingo', 'Chavez', '2100 Summer St NE', 'Minneapolis', 'MN', 55413, 'vendor', 'http://cravecatering.com/', 'Inspired by our passion for quality food and our love of helping guests create one of a kind memories, CRAVE Catering is a full service Minneapolis catering company dedicated to making your event unforgettable!'),
+('The Deco Catering', 'one', 'Hans', 'Gruber', '2010 E Hennepin Ave Ste 7', 'Minneapolis', 'MN', 55413, 'vendor', 'http://www.decocatering.com', 'Located in Minneapolis, The Deco Catering is a premier Minnesota caterer, founded by Soile Anderson in 1982. The Deco Catering offers award winning authentic European catering rich with flavor, artistic flair, and design.'),
+('Common Roots Catering', 'one', 'Bonnie', 'Patterson', '2558 Lyndale Ave S', 'Minneapolis', 'MN', 55405, 'vendor', 'https://www.commonrootscatering.com/', 'We are a full-service Minneapolis / St. Paul caterer providing our clients with good food made from scratch and exceptional service.  We will make your event memorable with our modern take on classic American cuisine using local and organic ingredients.'),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', ''),
+
+('Mintahoe', 'one', '', '', '', '', 'MN', , 'vendor', '', '');
 
 
 
@@ -31,19 +145,246 @@ VALUES (1, 'https://karaspartyideas.com/wp-content/uploads/2016/11/Bike-Themed-B
 INSERT INTO "vendors" ("vendorUserId", "companyName", "profilePic", "description", "additionalInfo", "phone")
 VALUES (2, 'Sir Knights Rentals', 'https://partycity6.scene7.com/is/image/PartyCity/_pdp_sq_?$_1000x1000_$&$product=PartyCity/819485', 'My company provides the realest Rennaissance experience you can get.', 'I am a small, family-run business.  You will be blown away by what good jesters my kids make!', '888-777-6666'),
 (4, 'Rockin Johns', 'https://www.rawmusictv.com/images/og-share-image/best-swedish-rock-bands.jpg', 'My Liberty Rockers will liven up your party no matter how small or large.', 'We are certified to play anywhere in the Kansas City metro.', '999-000-1234'),
-(6, 'The Magical Mushroomer', 'https://images.ctfassets.net/cnu0m8re1exe/4jaJ338Mbgu42tfIVnvWbp/e2c7e11773ff21b6d6f073fe8b87ece7/shutterstock_734992102.jpg?w=650&h=433&fit=fill', 'I have been rated the Most-Luscious Mushroom Grower 5 years running.', 'I am approved by the FDA.', '567-890-4567');
+(6, 'The Magical Mushroomer', 'https://images.ctfassets.net/cnu0m8re1exe/4jaJ338Mbgu42tfIVnvWbp/e2c7e11773ff21b6d6f073fe8b87ece7/shutterstock_734992102.jpg?w=650&h=433&fit=fill', 'I have been rated the Most-Luscious Mushroom Grower 5 years running.', 'I am approved by the FDA.', '567-890-4567'),
+(7, 'Mintahoe', 'https://www.mintahoe.com/wp-content/uploads/2017/04/MCE_Logo_White-e1492627487357.png', 'When you choose Mintahoe Catering &  Events, you choose pease of mind.  Our event experts will help you design the perfect menu with your vision and budget. Envision the Wow', 'We prioritize local products when available and cost-efficient to our clients.', '612-253-0255'),
+(8, 'Chowgirls', 'https://www.chowgirls.net/_interface/structure/chowgirls_large.png', 'Chowgirls creates beautiful, handcrafted fare using fresh, seasonal ingredients.', 'Strong relationships with local farmers and vendors allow our chefs to craft menus reflecting the tasts of each season and our commitment to sustainability.', '612-203-0786'),
+(9, 'Naf Naf Middle Eastern Grill', 'https://2utgaa2m4mzswgysf34qlzz1-wpengine.netdna-ssl.com/wp-content/uploads/logo.svg', 'We promise to be your guide to exploring the best flavors of the Middle East each time we make our handmade dishes, like pillowy pita, crispy falafel, savory shawarma, and our scratch-made signature sauces.', 'Middle Eastern cuisine done right.', '651-347-1119'),
+(10, 'CRAVE Catering & Events', 'http://cravecatering.com/wp-content/uploads/2017/02/CRAVE_CateringEvents_Logo_1_26_17_Black_RedV.png', 'Inspired by our passion for quality food and our love of helping guests create one of a kind memories, CRAVE Catering is a full service Minneapolis catering company dedicated to making your event unforgettable!', 'We offer Kosher meals', '952-562-5620'),
+(11, 'The Deco Catering', 'https://www.decocatering.com/wp-content/uploads/2015/11/deco_catering_logo.png', 'Located in Minneapolis, The Deco Catering is a premier Minnesota caterer, founded by Soile Anderson in 1982. The Deco Catering offers award winning authentic European catering rich with flavor, artistic flair, and design. With over 30 years of catering experience, The Deco Catering is a Twin Cities premier wedding caterer, corporate event caterer, and is proud to be one of a select few in Minnesota to carry a Kosher Food license.', 'Kosher Food license', '612-623-4477'),
+
+(12, 'Common Roots Catering', 'https://static1.squarespace.com/static/51018139e4b0d171103d3396/t/598b4efddb29d6084ce80dab/1609430941993/?format=1500w', 'We are a full-service Minneapolis / St. Paul caterer providing our clients with good food made from scratch and exceptional service.  We will make your event memorable with our modern take on classic American cuisine using local and organic ingredients. ', 'Local and sustainable food. Operating in an environmentaly responsible way. Values driven business.', '612-871-2360'),
+
+(13, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(14, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(15, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(16, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(17, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(18, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(19, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(20, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(21, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(22, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(23, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(24, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(25, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(26, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(27, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(28, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(29, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(30, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(31, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(32, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(33, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(34, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(35, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(36, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(37, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(38, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(39, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(40, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(41, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(42, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(43, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(44, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(45, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(46, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(47, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(48, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(49, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(50, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(51, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(52, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(53, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(54, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(55, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(56, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(57, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(58, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(59, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(60, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(61, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(62, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(63, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(64, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(65, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone'),
+
+(66, 'companyName', 'profilePic', 'description', 'additionalInfo', 'phone');
 
 --Fill vendors_features table with real special_features data for the fake vendors:
 INSERT INTO "vendors_features" ("vendorUserId", "featureId")
-VALUES (2, 3), (4, 3), (6, 1), (6, 3), (4, 3);
+VALUES (2, 3), (4, 3), (6, 1), (6, 3), (4, 3), (7, 3), (8,1), (9, 2), (11, 2),
+(12, 1),
+
+(13,),
+
+(14,),
+
+(15,),
+
+(16,),
+
+(17,),
+
+(18,),
+
+(19,),
+
+(20,),
+
+(21,),
+
+(22,),
+
+(23,),
+
+(24,),
+
+(25,),
+
+(26,),
+
+(27,),
+
+(28,),
+
+(29,),
+
+(30,),
+
+(31,),
+
+(32,),
+
+(33,),
+
+(34,),
+
+(35,),
+
+(36,),
+
+(37,),
+
+(38,),
+
+(39,),
+
+(40,),
+
+(41,),
+
+(42,),
+
+(43,),
+
+(44,),
+
+(45,),
+
+(46,),
+
+(47,),
+
+(48,),
+
+(49,),
+
+(50,),
+
+(51,),
+
+(52,),
+
+(53,),
+
+(54,),
+
+(55, ),
+
+(56, ),
+
+(57, ),
+
+(58, ),
+
+(59, ),
+
+(60, ),
+
+(61, ),
+
+(62, ),
+
+(63, ),
+
+(64, ),
+
+(65, ),
+
+(66, );
 
 --Fill vendors_services table with real service_types data for the fake vendors:
 INSERT INTO "vendors_services" ("vendorUserId", "serviceId")
-VALUES (2, 5), (2, 2), (4, 5), (4, 2), (6, 1), (6, 3);
-
-
-
-
+VALUES (2, 5), (2, 2), (4, 5), (4, 2), (6, 1), (6, 3), (7, 1), (8, 1), (9, 1),
+(10, 1), (11, 1), (12, 1), (13, 2), (14, 2), (15, 2), (16, 2), (17, 2), (18, 2),
+(19, 3), (20, 3), (21, 3), (22, 3), (23, 3), (24, 3), (25, 4), (26, 4), (27, 4),
+(28, 4), (29, 4), (30, 4), (31, 5), (32, 5), (33, 5), (34, 5), (35, 5), (36, 5),
+(37, 1), (38, 1), (39, 1), (40, 1), (41, 1), (42, 1), (43, 2), (44, 2), (45, 2),
+(46, 2), (47, 2), (48, 2), (49, 3), (50, 3), (51, 3), (52, 3), (53, 3), (54, 3),
+(55, 4), (56, 4), (57, 4), (58, 4), (59, 4), (60, 4), (61, 5), (62, 5), (63, 5), 
+(64, 5), (65, 5), (66, 5);
 
 --MESSAGES
 --Insert fake messages to start conversations:
