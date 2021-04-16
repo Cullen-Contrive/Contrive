4c65b36a8ad60b073eff415bbc364b7709b3cc3b
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-04-15 20:14:51 -0500

    initial checkbox setup, running into a wall

--- src/components/Profile/EditVendorSpecialFeatures.jsx
+++ src/components/Profile/EditVendorSpecialFeatures.jsx
@@ -1,7 +1,46 @@
-function EditVendorSpecialFeatures() {
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  Checkbox,
+  FormControl,
+  FormControlLabel,
+  FormGroup,
+  FormHelperText,
+  FormLabel,
+} from '@material-ui/core';
+
+function EditVendorSpecialFeatures({ editProfileElement, vendor }) {
+  const features = useSelector(store => store.features)
+
+  console.log('THESE ARE THE FEATURES', features)
   return(
-    <>
-    </>
+  <FormControl component="fieldset">
+    {features.map((feature, index) => {
+      let checked = false;
+
+      for (let specialFeature of vendor.special_features) {
+        if (specialFeature.id === feature.id) {
+          checked = true; 
+        }
+      }
+
+      return(
+        <FormControlLabel 
+          control={<Checkbox value={feature.id} checked={checked} color="primary" onChange={(event) => editProfileElement('special_features', event.target.value)} name="test" />} 
+          label={feature.name}
+          key={index}
+        />
+      );
+    })}
+    <FormControlLabel 
+      control={<Checkbox color="primary" checked={true} onChange={(event) => editProfileElement('special_features', event.target.value)} name="test" />} 
+      label="test checked"
+    />
+    <FormControlLabel 
+      control={<Checkbox color="primary" checked={false} onChange={() => editProfileElement('')} name="test" />} 
+      label="test unchecked"
+    />
+  </FormControl>
   );
 } // end EditVendorSpecialFeatures
 
