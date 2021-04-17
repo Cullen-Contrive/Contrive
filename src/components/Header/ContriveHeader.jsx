4868d6eab9a2382d8e634b683d4bba2efb867093
Dusty Meyers <dustymeyerssculpture@gmail.com>
2021-04-17 15:06:48 -0500

    added some conditional rendering for header

--- src/components/Header/ContriveHeader.jsx
+++ src/components/Header/ContriveHeader.jsx
@@ -1,33 +1,41 @@
-import Box from '@material-ui/core/Box';
+import { useSelector } from 'react-redux';
+
+// Material-UI components
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
 
+// Custom Components
 import contriveLogo from '../Images/contriveLogo.png';
 import useStyles from './Header.styles';
 
 function ContriveHeader() {
   const classes = useStyles();
+  const user = useSelector((store) => store.user);
 
   return (
-    <Grid container direction="row" alignItems="center" justifyContent="flex-start" spacing={0} className={classes.contriveHeader}>
-      <Grid item xs={4}>
-        <img
-          className={classes.headerLogo}
-          src={contriveLogo}
-        />
-      </Grid>
-      
-      <Grid item xs ={8}>
-        <Typography
-          variant="h2"
-          component="h1"
-          className={classes.headerText}
-        >
-          Contrive
-        </Typography>
-      </Grid>
-
-    </Grid>
+    <> {/* if user is not logged in, header will not render */}
+      {user.id && (
+        <Grid container direction="row" alignItems="center" justifyContent="flex-start" spacing={0} className={classes.contriveHeader}>
+          <Grid item xs={4}>
+            <img
+              className={classes.headerLogo}
+              src={contriveLogo}
+            />
+          </Grid>
+          
+          <Grid item xs ={8}>
+            <Typography
+              variant="h2"
+              component="h1"
+              className={classes.headerText}
+            >
+              Contrive
+            </Typography>
+          </Grid>
+    
+        </Grid>
+      )}
+    </>
   );
 }
 
