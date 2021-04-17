68e1a22780c168160039af23ea3c79f44c306005
Johnshandsable <Jshands@asu.edu>
2021-04-17 13:03:14 -0500

    cleaning up chat box

--- src/components/MessageAll/MessageAll.jsx
+++ src/components/MessageAll/MessageAll.jsx
@@ -9,20 +9,46 @@ import DateObject from 'react-date-object';
 import Swal from 'sweetalert2';
 
 // Material UI
-import Button from '@material-ui/core/Button';
-import Card from '@material-ui/core/Card';
-import Grid from '@material-ui/core/Grid';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
 import Typography from '@material-ui/core/Typography';
-import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import Fab from '@material-ui/core/Fab';
+import SendIcon from '@material-ui/icons/Send';
 
 // Custom Components
 import Message from './Message';
+import { FavoriteBorder } from '@material-ui/icons';
 
 const Form = styled.form`
   width: 400px;
 `;
+
+const useStyles = makeStyles({
+  table: {
+    minWidth: 650,
+  },
+  chatSection: {
+    width: '100%',
+    height: '80vh',
+  },
+  headBG: {
+    backgroundColor: '#e0e0e0',
+  },
+  borderRight500: {
+    borderRight: '1px solid #e0e0e0',
+  },
+  messageArea: {
+    height: '70vh',
+    overflowY: 'auto',
+  },
+});
 
 function MessageAll() {
   const [message, setMessage] = useState('');
@@ -107,56 +133,57 @@ function MessageAll() {
     history.push('/messages');
   };
 
+  const classes = useStyles();
+
   return (
-    <Grid container spacing={3}>
-      <Grid item xs={12}>
-        <Button startIcon={<ArrowBackIosIcon />} onClick={goBack}></Button>
-        <span>
-          <Typography variant="h3">Conversation Name Goes Here</Typography>
-        </span>
+    <div>
+      <Grid container>
+        <Grid item xs={12}>
+          <Typography variant="h5" className="header-message">
+            Chat
+          </Typography>
+        </Grid>
       </Grid>
-      <Grid item xs={12}>
-        <Paper
-          style={{
-            backgroundColor: '#fe67',
-            height: 300,
-            marginLeft: 10,
-            marginRight: 10,
-          }}
-        >
-          {existingMessages.length > 0 ? (
-            existingMessages.map((singleMessage, index) => {
-              return (
-                <Message
-                  key={index}
-                  messageDetails={singleMessage}
-                  toUserId={params.id}
-                  currentUser={currentUser}
-                  toUser={toUser}
+      <Grid container component={Paper} className={classes.chatSection}>
+        <Grid item xs={9}>
+          <List className={classes.messageArea}>
+            {existingMessages.length > 0 ? (
+              existingMessages.map((singleMessage, index) => {
+                return (
+                  <Message
+                    key={index}
+                    messageDetails={singleMessage}
+                    toUserId={params.id}
+                    currentUser={currentUser}
+                    toUser={toUser}
+                  />
+                );
+              })
+            ) : (
+              <ListItemText align="left">Start a conversation!</ListItemText>
+            )}
+          </List>
+          <form onSubmit={sendMessage}>
+            <Grid container style={{ padding: '20px' }}>
+              <Grid item xs={11}>
+                <TextField
+                  id="outlined-basic-email"
+                  label="Type Something"
+                  value={message}
+                  onChange={(evt) => setMessage(evt.target.value)}
+                  fullWidth
                 />
-              );
-            })
-          ) : (
-            <Typography>Start a conversation!</Typography>
-          )}
-        </Paper>
+              </Grid>
+              <Grid xs={1} align="right">
+                <Fab color="primary" aria-label="add" type="submit">
+                  <SendIcon />
+                </Fab>
+              </Grid>
+            </Grid>
+          </form>
+        </Grid>
       </Grid>
-      {/* Form for submitting text to another user */}
-      <Grid item xs={12}>
-        <Form onSubmit={sendMessage}>
-          <TextField
-            value={message}
-            style={{ margin: 8 }}
-            fullWidth
-            onChange={(evt) => setMessage(evt.target.value)}
-            placeholder="Say something"
-          ></TextField>
-          <Button type="submit" color="primary" variant="contained">
-            Send Message
-          </Button>
-        </Form>
-      </Grid>
-    </Grid>
+    </div>
   );
 }
 
