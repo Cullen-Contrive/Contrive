5f0e1688b3274ea7baff024c9b67dafc347464e0
William Krug <william.p.krug@gmail.com>
2021-04-19 10:26:51 -0500

    removed "Languages" (globe) icon from bottom nav and replaced with "explore" (compass) icon

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,7 @@ import { Link, useHistory } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
 import './Nav.css';
 
-import useStyles from './Nav.styles'
+import useStyles from './Nav.styles';
 import { useSelector } from 'react-redux';
 
 // Material-UI Components
@@ -19,11 +19,11 @@ import {
   List,
   ListItem,
   ListItemText,
-  Typography
+  Typography,
 } from '@material-ui/core';
 
 // Material-UI Icons
-import LanguageIcon from '@material-ui/icons/Language';
+import ExploreIcon from '@material-ui/icons/Explore';
 import SearchIcon from '@material-ui/icons/Search';
 import AddCircleIcon from '@material-ui/icons/AddCircle';
 import ChatIcon from '@material-ui/icons/Chat';
@@ -48,13 +48,13 @@ function Nav() {
 
   // Handles menu icon change when nav bar icons are clicked
   const handleChange = (event, newValue) => {
-    console.log('handleChange Nav', newValue)
+    console.log('handleChange Nav', newValue);
     setValue(newValue);
     if (newValue === 'menu') {
       return toggleDrawer(true);
     }
     history.push(`${newValue}`);
-  }
+  };
 
   // Opens Menu Drawer
   const toggleDrawer = (open) => {
@@ -64,52 +64,111 @@ function Nav() {
   };
 
   return (
-
-    <> {/* if user is not logged in, bottom nav will not render */}
+    <>
+      {' '}
+      {/* if user is not logged in, bottom nav will not render */}
       {user.id && (
         <BottomNavigation
           value={value}
           onChange={handleChange}
           className={classes.navbar}
         >
+          <BottomNavigationAction
+            label="Discover"
+            value="/discover"
+            icon={<ExploreIcon />}
+          />
+          <BottomNavigationAction
+            label="Search"
+            value="/search"
+            icon={<SearchIcon />}
+          />
+          <BottomNavigationAction
+            label="Plan"
+            value="/events/create"
+            icon={
+              <AddCircleIcon fontSize="large" style={{ color: '#B38208' }} />
+            }
+          />
+          <BottomNavigationAction
+            label="Messages"
+            value="/messages"
+            icon={<ChatIcon />}
+          />
+          <BottomNavigationAction
+            label="Menu"
+            value="menu"
+            icon={<MenuIcon />}
+          />
 
-          <BottomNavigationAction label="Discover" value="/discover" icon={<LanguageIcon />} />
-          <BottomNavigationAction label="Search" value="/search" icon={<SearchIcon />} />
-          <BottomNavigationAction label="Plan" value="/events/create" icon={<AddCircleIcon fontSize="large" style={{ color: '#B38208' }} />} />
-          <BottomNavigationAction label="Messages" value="/messages" icon={<ChatIcon />} />
-          <BottomNavigationAction label="Menu" value="menu" icon={<MenuIcon />} />
-
-          <Drawer anchor="right" open={state} onClose={() => toggleDrawer(false)} className={classes.menuDrawer}>
+          <Drawer
+            anchor="right"
+            open={state}
+            onClose={() => toggleDrawer(false)}
+            className={classes.menuDrawer}
+          >
             <Box p={1}>
-              <Typography variant='h6' component="h2" gutterBottom>
+              <Typography variant="h6" component="h2" gutterBottom>
                 {user.firstName}'s Dashboard
               </Typography>
             </Box>
 
             {/* Todo - add the user's profile photo here */}
             <Box p={2}>
-              <center><Avatar>C</Avatar></center>
+              <center>
+                <Avatar>C</Avatar>
+              </center>
             </Box>
 
             <Divider />
 
             <Box p={2}>
-              <Grid container direction="column" spacing={2} alignItems="center">
+              <Grid
+                container
+                direction="column"
+                spacing={2}
+                alignItems="center"
+              >
                 <Grid item>
-                  <Button color="primary" onClick={() => history.push('/myProfile')}>My Profile</Button>
+                  <Button
+                    color="primary"
+                    onClick={() => history.push('/myProfile')}
+                  >
+                    My Profile
+                  </Button>
                 </Grid>
                 <Grid item>
-                  <Button color="primary" onClick={() => history.push('/myNetwork')}>My Network</Button>
+                  <Button
+                    color="primary"
+                    onClick={() => history.push('/myNetwork')}
+                  >
+                    My Network
+                  </Button>
                 </Grid>
                 <Grid item>
-                  <Button color="primary" onClick={() => history.push('/myEvents')}>My Events</Button>
+                  <Button
+                    color="primary"
+                    onClick={() => history.push('/myEvents')}
+                  >
+                    My Events
+                  </Button>
                 </Grid>
                 <Grid item>
-                  <Button color="primary" onClick={() => history.push('/myCalendar')}>My Calendar</Button>
+                  <Button
+                    color="primary"
+                    onClick={() => history.push('/myCalendar')}
+                  >
+                    My Calendar
+                  </Button>
                 </Grid>
                 {user.type === 'planner' && (
                   <Grid item>
-                    <Button color="primary" onClick={() => history.push('/inspiration')}>Inspiration</Button>
+                    <Button
+                      color="primary"
+                      onClick={() => history.push('/inspiration')}
+                    >
+                      Inspiration
+                    </Button>
                   </Grid>
                 )}
                 <Grid item>
@@ -117,7 +176,6 @@ function Nav() {
                 </Grid>
               </Grid>
             </Box>
-
           </Drawer>
         </BottomNavigation>
       )}
