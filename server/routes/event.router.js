74a8a72909c05e128862dd5f32ec44ae23d8def6
Johnshandsable <Jshands@asu.edu>
2021-04-19 14:10:19 -0500

    added post route for creating events

--- server/routes/event.router.js
+++ server/routes/event.router.js
@@ -58,8 +58,84 @@ router.get('/', (req, res) => {
 });
 
 /**
+ * GET route for /api/event/id
+ */
+router.get('/:id', (req, res) => {
+  // Grabs an event by id
+  const queryText = `
+  SELECT 
+  "events"."id",
+  "events"."dateOfEvent", 
+  "events"."timeOfEvent", 
+  "events"."address", 
+  "events"."city", 
+  "events"."state", 
+  "events"."zip",
+  "events"."numberOfAttendees",
+  "events"."description",
+  "vendors"."companyName",
+  "users"."firstName",
+  "users"."lastName",
+  JSON_AGG("events_photos".*) AS "eventsPhotos", 
+  JSON_AGG("events_types".*) AS "eventsTypes", 
+  JSON_AGG("events_vendors".*) AS "eventsVendors"
+  FROM "events"
+  LEFT OUTER JOIN "events_photos" ON "events"."id" = "events_photos"."eventId" 
+  LEFT OUTER JOIN "events_types" ON "events"."id" = "events_types"."eventId" 
+  LEFT OUTER JOIN "types_of_event" ON "events_types"."typeId" = "types_of_event"."id"
+  LEFT OUTER JOIN "events_vendors" ON "events"."id" = "events_vendors"."eventId"
+  LEFT OUTER JOIN "vendors" ON "events_vendors"."vendorUserId" = "vendors"."vendorUserId"
+  LEFT OUTER JOIN "users" ON "users"."id" = "vendors"."vendorUserId"
+  WHERE "events"."id" = $1
+  GROUP BY 
+  "events"."id",
+  "events"."dateOfEvent", 
+  "events"."timeOfEvent", 
+  "events"."address", 
+  "events"."city", 
+  "events"."state", 
+  "events"."zip",
+  "events"."numberOfAttendees",
+  "events"."description",
+  "vendors"."companyName", 
+  "users"."firstName",
+  "users"."lastName";`;
+  const eventId = req.params.id;
+
+  pool
+    .query(queryText, [eventId])
+    .then((dbRes) => {
+      console.log('SERVER - GET events at /api/event/id successful!');
+      // send back an object rather than an array because return should be only one value
+      res.send(dbRes.rows[0]);
+    })
+    .catch((err) => {
+      console.error(
+        'SERVER - GET events at /api/event/id an error occurred',
+        err
+      );
+      res.sendStatus(500);
+    });
+});
+
+/**
  * POST route for /api/event
  */
-router.post('/', (req, res) => {});
+router.post('/', (req, res) => {
+  const queryText = `
+  INSERT INTO "events" 
+  ("plannerUserId", 
+  "dateOfEvent", 
+  "timeOfEvent", 
+  "address", 
+  "city", 
+  "state", 
+  "zip", 
+  "numberOfAttendees", 
+  "description")
+  VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9);`;
+
+  pool.query(queryText);
+});
 
 module.exports = router;
