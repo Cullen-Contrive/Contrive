d56ff72c586eba5449852649249ae8c3b24c60f5
Johnshandsable <Jshands@asu.edu>
2021-04-19 13:03:52 -0500

    setup template for event router

---
+++ server/routes/event.router.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+/**
+ * GET route
+ */
+router.get('/', (req, res) => {
+  // GET route code here
+});
+
+/**
+ * POST route template
+ */
+router.post('/', (req, res) => {
+  // POST route code here
+});
+
+module.exports = router;
