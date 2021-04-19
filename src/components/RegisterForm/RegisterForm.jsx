0b690980a5e426b596cf080fa21ced21b49a9474
Jason Lin <jasonwl_1995@yahoo.com>
2021-04-18 22:59:05 -0500

    functional ImageUpload completed

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import ImageUpload from '../ImageUpload/ImageUpload';
+import ImageUpload from '../ImageUpload/ImageUploadFunctional';
 
 // Material-UI
 import {
