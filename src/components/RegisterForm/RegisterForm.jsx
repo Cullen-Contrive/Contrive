0d23d7cb5da63b160738aabd17094d31a95bd699
Jason Lin <jasonwl_1995@yahoo.com>
2021-04-20 12:21:04 -0500

    fixed image upload import on <RegisterForm.jsx>

--- src/components/RegisterForm/RegisterForm.jsx
+++ src/components/RegisterForm/RegisterForm.jsx
@@ -1,8 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { useHistory } from 'react-router-dom';
-import ImageUpload from '../ImageUpload/ImageUploadFunctional';
 import useStyles from './RegisterForm.styles';
+import ImageUpload from '../ImageUpload/ImageUpload';
 
 // Material-UI
 import {
