d1cfa6555a23ea3c9cb289496f9acbaa5f4dec74
Johnshandsable <Jshands@asu.edu>
2021-04-22 19:49:38 -0500

    added a way to see event description on myevents page

--- src/components/Event/MyEvents.jsx
+++ src/components/Event/MyEvents.jsx
@@ -7,6 +7,8 @@ import ListItemAvatar from '@material-ui/core/ListItemAvatar';
 import ListItemText from '@material-ui/core/ListItemText';
 import Typography from '@material-ui/core/Typography';
 
+import placeholderImage from '../Images/placeholder.png';
+
 function MyEvents() {
   const dispatch = useDispatch();
   const myEvents = useSelector((store) => store.events.allEventsReducer);
@@ -24,7 +26,29 @@ function MyEvents() {
       <Grid item xs={12}>
         <Typography variant="h2">My Events</Typography>
       </Grid>
-      <Grid item xs={12}></Grid>
+      <Grid item xs={12}>
+        {/* Events go here! */}
+        {myEvents.map((event, index) => {
+          return (
+            <ListItem key={index}>
+              <ListItemAvatar
+                src={
+                  event.eventsPhotos[0] !== null
+                    ? event.eventsPhotos[0].photo
+                    : { placeholderImage }
+                }
+                alt={event.description ? event.description + 'photo' : ''}
+              />
+              <ListItemText>
+                {event.description ? event.description : ''}
+              </ListItemText>
+              <ListItemText>
+                {event.dateOfEvent ? event.dateOfEvent : ''}
+              </ListItemText>
+            </ListItem>
+          );
+        })}
+      </Grid>
     </Grid>
   );
 }
