98a44c80e14cb691ea32cc33e4baf03c9dac1e7a
Johnshandsable <Jshands@asu.edu>
2021-04-29 15:08:47 -0500

    removed socket ports

--- server/server.js
+++ server/server.js
@@ -3,15 +3,6 @@ const bodyParser = require('body-parser');
 require('dotenv').config();
 
 const app = express();
-const http = require('http').Server(app);
-const io = require('socket.io')(http, {
-  cors: {
-    // required: Cors is required for other browsers including Firefox
-    origin: 'http://localhost:3000', // required: used for proxying requests coming from client-side.
-    // Cannot access this port without specifically being from client-side.
-    methods: ['GET', 'POST'],
-  },
-});
 
 const sessionMiddleware = require('./modules/session-middleware');
 const passport = require('./strategies/user.strategy');
@@ -80,32 +71,6 @@ app.use(express.static('build'));
 
 // App Set //
 const PORT = process.env.PORT || 5000;
-
-// Whenever someone connects this gets executed
-// socket.io used for scalability. If a chat can have more than
-// two users, socket.io will be able to track all users within a room
-// then we can decide whether or not to post those messages to database.
-
-io.on('connection', (socket) => {
-  console.log('A user connected!');
-
-  socket.on('disconnect', () => {
-    console.log('User has disconnected!');
-  });
-
-  socket.on('join', ({ name, room }) => {
-    console.log('User has joined chat!', name, room);
-  });
-
-  socket.on('send message', (message) => {
-    console.log('Got a message', message);
-    io.emit('send message', message);
-  });
-});
-
-http.listen(4000, function () {
-  console.log('listening on port 4000');
-});
 
 /** Listen * */
 app.listen(PORT, () => {
